da08554b5074c3935824893a0ef7a9cb89a0cb68
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-03-15 15:38:26 +0100

    add navbar

--- cat_watch/assets-php/navBar.php
+++ cat_watch/assets-php/navBar.php
@@ -1,9 +1,19 @@
-<nav>
-    
-</nav>
-
-<header>
-
-<h1>Cat Watch?</h1>
-
-</header>
+<div>
+        <img id="logo" src="logo.png" alt "logo">
+        
+        <nav>
+            <ul>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Conseils &ensp;</a></li>
+                <li class="deroulant"><a href="#">Articles &ensp;</a>
+                <ul class="sous">
+                  <li><a href="#">Section 1</a></li>
+                  <li><a href="#">Section 2</a></li>
+                  <li><a href="#">Section 3</a></li>
+                </ul>
+              </li>
+              
+              
+            </ul>
+          </nav>
+        </div>
