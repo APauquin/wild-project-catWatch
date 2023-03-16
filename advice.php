bb7416f106ebf4feefa5e75c0df149935935dcdf
APauquin <111913188+APauquin@users.noreply.github.com>
2023-03-16 10:22:07 +0100

    updated advice

--- advice.php
+++ advice.php
@@ -1,1 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <link rel="stylesheet" href="assets-css/styleAdvice.css" />
+  <title>Conseils</title>
+</head>
+
+<body>
+  <header>
+    <?php include("assets-php/navBar.php")
+    ?> 
+  </header>
+
+  <h2>CONSEILS</h2>
+
+  <section class="main">
+    <p class="intro">
+      Élever un chat robot peut être une expérience amusante et intéressante,
+      mais cela nécessite de la patience et des connaissances techniques en
+      robotique.<br />
+      Voici quelques étapes à suivre pour élever un chat robot :
+    </p>
+    <p>
+      1- Trouvez un kit de construction de robot : il existe de nombreux kits
+      de construction de robot disponibles sur le marché. Recherchez des kits
+      spécialement conçus pour construire un chat robot. <br />
+      2- Assemblez le chat robot : Suivez les instructions fournies avec le
+      kit pour assembler le chat robot. Cela implique généralement de
+      connecter les différentes parties du robot, comme le corps, la tête, les
+      pattes et la queue.<br />
+      3- Programmez le chat robot : Une fois que vous avez assemblé le chat
+      robot, vous devez le programmer. La plupart des kits de robotique
+      viennent avec un logiciel de programmation. Vous pouvez également
+      trouver des tutoriels en ligne pour vous aider à apprendre à programmer
+      votre robot.<br />
+      4- Entraînez le chat robot : Une fois que vous avez programmé le chat
+      robot, vous devez l'entraîner. Vous pouvez le faire en utilisant des
+      capteurs pour détecter les objets et les obstacles, en enseignant au
+      robot à se déplacer et à se comporter comme un chat. <br />
+      5- Amusez-vous avec votre chat robot : Une fois que vous avez terminé de
+      construire et de programmer votre chat robot, vous pouvez passer du
+      temps à jouer avec lui et à l'observer interagir avec son environnement.
+    </p>
+    <p class="conclusion">
+      En résumé, pour élever un chat robot, il faut assembler le robot, le
+      programmer, l'entraîner et passer du temps à jouer avec lui.<br />
+      Cependant n'oubliez pas de verifier régulierement le niveau d'huile
+      ainsi que l'état géneral de votre chat robot (notamment pour les chattes
+      à roulettes dont les roulement s'usent rapidement).
+    </p>
+  </section>
+
+  <?php
+  include("assets-php/footer.php")
+  ?>
+
+</body>
+
+</html>
