bbfd6f98550f84b82725cdf20da7eb0ce22df4c8
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-03-17 11:55:42 +0100

    change nav bar

--- assets-php/navBar.php
+++ assets-php/navBar.php
@@ -1,33 +1,23 @@
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="assets-css/styleNav.css">
-  <title>footer</title>
-</head>
-<body>
-<div id="navbar">
-          <img id="logo" src="https://i.ibb.co/qysGLcN/Illustration-sans-titre-1.png" alt="Illustration-sans-titre-1" border="0">
-        
-          <nav>
-            <ul>
-                <li><a href="./contact.php">Contact</a></li>
-                <li><a href="./conseil.php">Conseils</a></li>
-                <li class="deroulant"><a href="#">Articles</a>
-                <ul class="sous">
-                  <li><a href="#">Section 1</a></li>
-                  <li><a href="#">Section 2</a></li>
-                  <li><a href="#">Section 3</a></li>
+<nav>
+        <a href "./index.php"><img id="logo" src="https://i.ibb.co/qysGLcN/Illustration-sans-titre-1.png"
+            alt="Illustration-sans-titre-1" border="0"></a>
+        <ul>
+            <li id="vide"></li>
+            <li class="menu-deroulant">
+                <a href="#">Articles</a>
+                <ul class="sous-menu">
+                    <li><a href="#">categories</a></li>
+                    <li><a href="#">categories</a></li>
+                    <li><a href="#">categories</a></li>
                 </ul>
-              </li>
-              
-              
-            </ul>
-          </nav>
-        </div>
-</div>
-</body>
-</html>
+            </li>
+
+            <li><a href="#">Conseils</a></li>
+            <li><a href="#">Contacts</a></li>
+
+        </ul>
+    </nav>
+    <div>
+
+    </div>
