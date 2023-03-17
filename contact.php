5afa4f1c1329e4185c9e4b7d1c58b2d0bca7be5e
APauquin <111913188+APauquin@users.noreply.github.com>
2023-03-17 11:21:28 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,22 +5,31 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./extern.css">
+
+    <link rel="stylesheet" href="./assets/styleContact.css">
 </head>
-	
-<blockNav>
-<div class="background">
-    <div class="container">
-      <div class="screen">
-        <div class="screen-body">
-          <div class="screen-body-item left">
-            <div class="app-title">
-              <span>CONTACT US</span>
-            </div>
-            <div class="app-contact">CONTACT INFO : 118 218</div>
-          </div>
-          <div class="screen-body-item">
-            <div class="app-form">
+
+<body>
+  <header>
+    <?php include("assets-php/contact.php")
+    ?> 
+  </header>
+
+  <h2>Contact</h2>
+
+  <section>
+    <div class="background">
+      <div class="container">
+        <div class="screen">
+          <div class="screen-body">
+            <div class="screen-body-item left">
+              <div class="app-title">
+                <span>CONTACT US</span>
+              </div>
+              <div class="app-contact">CONTACT INFO : 118 218</div>
+              </div>
+              <div class="screen-body-item">
+              <div class="app-form">
               <div class="app-form-group">
                 <input class="app-form-control" placeholder="NAME" value="Name">
               </div>
@@ -33,12 +42,22 @@
               <div class="app-form-group buttons">
                 <button class="app-form-button">CANCEL</button>
                 <button class="app-form-button">SEND</button>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-  </div>
-</blockNav> 
-</html>
+  </section>
+</body>
+
+<h3>
+    <img src="https://fakeimg.pl/100x100/">
+    <img src="https://fakeimg.pl/100x100/">
+    <img src="https://fakeimg.pl/100x100/">
+    <img src="https://fakeimg.pl/100x100/">
+    <img src="https://fakeimg.pl/100x100/">
+</h3>
+
+
