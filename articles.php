178974a0d9081c6c44381bb48e10d3d0b6519c49
APauquin <111913188+APauquin@users.noreply.github.com>
2023-03-17 14:42:31 +0100

    Create articles.php

---
+++ articles.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="./assets-css/style.css">
+
+    <title>Cat Watch</title>
+</head>
+<body>
+    <!-- article list -->
+    </section>
+    
+        <section class="last-posts" id="last-posts">
+            <h2>Our latest posts</h2>
+            
+            <article>
+                <h2>Article1</h2>
+                <ul>
+                        <li>
+                            <h5>Merlin:</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </li>
+                         
+                    </ul>
+            </article>
+            <article>
+                <h2>Article2</h2>
+                 <ul>
+                        <li>
+                            <h5>Perceval:</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </li>
+                         
+                    </ul>
+            </article>
+            <article>
+                <h2>Article3</h2>
+                 <ul>
+                        <li>
+                            <h5>Nimue:</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </li>
+                         
+                    </ul>
+            </article>
+            <article>
+                <h2>Article4</h2>
+                 <ul>
+                        <li>
+                            <h5>Phoebe:</h5>
+                            <p>
+                                Lorem ipsum dolor sit amet, consectetur adipisci elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+                            </p>
+                        </li>
+                         
+                    </ul>
+            </article>
+        </section>
+    <!-- end of article list -->
+</body>
+</html>
