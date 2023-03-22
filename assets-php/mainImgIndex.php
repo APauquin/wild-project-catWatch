5dc6d8acd860b57760186ec4ab50422225a9ae88
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-03-22 15:58:39 +0100

    nyan cat move to index only, footer modified in class in css, fixed shortcut logo index

--- assets-php/mainImgIndex.php
+++ assets-php/mainImgIndex.php
@@ -1,18 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-
-    <link rel="stylesheet" href="./assets-css/style.css">
-</head>
-
-<body class="mainImgContainer">
+<div class="mainImgContainer">
     <img class="indexMainImg" src="../assets-img/1000_F_540542211_cst3aGs95TS0cEZhb2MgMiticC7rozBz.jpg" alt="Robot cat facing off a organic cat through a window">
     <div class="centered">CAT WATCH</div>
-</body>
-
-</html>
+</div>
