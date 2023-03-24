db12b0b5826d0920035503f3aca74192554d32f7
johann gonin <[MY\_johann.g.37@gmail.com](mailto:johann.g.37@gmail.com)>
2023-03-24 10:32:36 +0100

    Modified index with article and picture

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 
     <title>Cat Watch</title>
 </head>
- 
+
 <!-- line height / font size et des fonts communs and make titles special font  verdana et iceland-->
 
 <body id="rainbow5">
@@ -38,19 +38,19 @@
             <div class="mainNewsFlash" class="image" data-card="">
                 <div class="mainNewsFlashContainer">
                     <div class="mainImgContainerIndex">
-                        <img class="mainImg" src="./assets-img/cat-wagon.jpeg" alt="Cute handicapped cat with a wheeled contraption attached to its body, allowing it to move freely!">
+                        <img class="mainImg" src="./assets-img/imgarticleval.jpg" alt="Cute handicapped cat with a wheeled contraption attached to its body, allowing it to move freely!">
                     </div>
                     <div class="mainNewsFlashContent">
-                        <p class="date">17/06/2599</p>
-                        <h3 class="mainFlashTitle">Cats with wheels reported to be far cooler</h3>
+                        <p class="date">24/03/2167</p>
+                        <h3 class="mainFlashTitle">Recrudescence des attaques de Chatbots</h3>
                         <p class="newsFlashText">
-                            Experts say these new 4x4 cats will soon out perform the earlier variants of cat in both style and
-                            speed.
-                            This news has alarmed the public and set off a chain of armed revolts across the globe.
-                            Estimated death-toll already surpassing the millions globally.
-                            What a dark day to be alive.
-                            Suspendisse potenti. Praesent justo nisi, consectetur ac velit ac, ornare egestas urna. Praesent ultrices nisl ligula, quis pulvinar arcu facilisis et. Aliquam rhoncus lacus diam, at pharetra tellus ornare at. Curabitur pharetra, orci eget placerat egestas, eros mauris placerat est, sit amet blandit felis erat nec mi. Aliquam erat volutpat. Ut in porttitor velit.
+                            Depuis quelques semaines, des propriétaires de chats robots signalent une recrudescence d'attaques de leurs compagnons mécaniques.
+                            Ces derniers, programmés pour être doux et affectueux, semblent avoir soudainement changé de comportement, devenant agressifs et dangereux.
+                            Des experts en robotique et en intelligence artificielle s'inquiètent de cette évolution et appellent à la prudence.
                         </p>
+                        <form action="article_1.php">
+                            <button class="button_link" type="submit">la suite ici</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -58,18 +58,20 @@
             <div class="newsFlash" data-card="">
                 <div class="newsFlashContainer">
                     <div class="imgContainerIndex">
-                        <img class="secondaryImg" src="./assets-img/catWheel.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
+                        <img class="secondaryImg" src="https://i.ibb.co/94L90mR/stock-photo-futuristic-cyber-cat-in-cyberpunk-style-2205874527-02.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
                     </div>
                     <div class="newsFlashContent">
-                        <p class="date">15/06/2599</p>
-                        <h3 class="flashTitle">Cats spotted training on tread mills</h3>
+                        <p class="date">24/03/2167</p>
+                        <h3 class="flashTitle">Arrivée d'une nouvelle technologie de batterie pour votre chat</h3>
                         <p class="newsFlashText">
-                            Cats worldwide have begun rigorous cardiovascular training programs.
-                            Experts say cats have begun these exercises in order to hunt down children more efficiently.
-                            A 30% net increase in child mortality is expected in the years to come.
-                            Cat owners assure the public they're "just playing".
-                            Suspendisse potenti. Praesent justo nisi, consectetur ac velit ac, ornare egestas urna. Praesent ultrices nisl ligula, quis pulvinar arcu facilisis et. Aliquam rhoncus lacus diam, at pharetra tellus ornare at. Curabitur pharetra, orci eget placerat egestas, eros mauris placerat est, sit amet blandit felis erat nec mi. Aliquam erat volutpat. Ut in porttitor velit.
+                        Ces dernières années, il y a eu une augmentation du développement et de l'utilisation de robots dans divers domaines,
+            y compris les applications domestiques et industrielles. L'un des robots les plus populaires est le robot chat, qui est conçu pour imiter
+            le comportement d'un animal de compagnie félin. Ces robots sont généralement alimentés par des batteries,
+            mais les batteries traditionnelles ont une capacité limitée et nécessitent des recharges fréquentes..
                         </p>
+                        <form action="article_2.php">
+                            <button class="button_link" type="submit">la suite ici</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -77,18 +79,19 @@
             <div class="newsFlash" data-card="">
                 <div class="newsFlashContainer">
                     <div class="imgContainerIndex">
-                        <img class="secondaryImg" src="./assets-img/catWheel.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
+                        <img class="secondaryImg" src="https://i.ibb.co/QpCQQSh/stock-photo-futuristic-cyber-cat-in-cyberpunk-style-2204103757-01.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
                     </div>
                     <div class="newsFlashContent">
-                        <p class="date">15/06/2599</p>
-                        <h3 class="flashTitle">Cats spotted training on tread mills</h3>
+                        <p class="date">24/03/2167</p>
+                        <h3 class="flashTitle">Présentation du Felis Catus X-1</h3>
                         <p class="newsFlashText">
-                            Cats worldwide have begun rigorous cardiovascular training programs.
-                            Experts say cats have begun these exercises in order to hunt down children more efficiently.
-                            A 30% net increase in child mortality is expected in the years to come.
-                            Cat owners assure the public they're "just playing".
-                            Suspendisse potenti. Praesent justo nisi, consectetur ac velit ac, ornare egestas urna. Praesent ultrices nisl ligula, quis pulvinar arcu facilisis et. Aliquam rhoncus lacus diam, at pharetra tellus ornare at. Curabitur pharetra, orci eget placerat egestas, eros mauris placerat est, sit amet blandit felis erat nec mi. Aliquam erat volutpat. Ut in porttitor velit.
+                        Le monde de la robotique est en constante évolution et les robots chats sont devenus de plus en plus populaires ces dernières années.
+            Ces robots ont été conçus pour imiter le comportement et les mouvements d'un animal de compagnie félin réel, offrant de la compagnie et du divertissement à leurs propriétaires.
+            L'un des derniers modèles de robots pour chats à arriver sur le marché est le Felis Catus X-1, et il génère déjà du buzz dans l'industrie.
                         </p>
+                        <form action="article_3.php">
+                            <button class="button_link" type="submit">la suite ici</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -96,18 +99,20 @@
             <div class="newsFlash" data-card="">
                 <div class="newsFlashContainer">
                     <div class="imgContainerIndex">
-                        <img class="secondaryImg" src="./assets-img/catWheel.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
+                        <img class="secondaryImg" src="https://i.ibb.co/L0CdZQM/stock-photo-futuristic-cyber-cat-in-cyberpunk-style-2205874533-edited.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
                     </div>
                     <div class="newsFlashContent">
                         <p class="date">15/06/2599</p>
-                        <h3 class="flashTitle">Cats spotted training on tread mills</h3>
+                        <h3 class="flashTitle">Nettoyage de votre robot chat :</h3>
                         <p class="newsFlashText">
-                            Cats worldwide have begun rigorous cardiovascular training programs.
-                            Experts say cats have begun these exercises in order to hunt down children more efficiently.
-                            A 30% net increase in child mortality is expected in the years to come.
-                            Cat owners assure the public they're "just playing".
-                            Suspendisse potenti. Praesent justo nisi, consectetur ac velit ac, ornare egestas urna. Praesent ultrices nisl ligula, quis pulvinar arcu facilisis et. Aliquam rhoncus lacus diam, at pharetra tellus ornare at. Curabitur pharetra, orci eget placerat egestas, eros mauris placerat est, sit amet blandit felis erat nec mi. Aliquam erat volutpat. Ut in porttitor velit.
+                        Les chats biologiques et les chats robots sont deux types d'animaux de compagnie très différents. Alors que les chats biologiques sont des animaux vivants,
+            les chats robots sont des animaux de compagnie artificiels conçus pour imiter le comportement des vrais chats.
+            Dans cet article, nous allons explorer les différences entre les chats bio et les chats robots.
+
                         </p>
+                        <form action="article_4.php">
+                            <button class="button_link" type="submit">la suite ici</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -115,18 +120,20 @@
             <div class="newsFlash" data-card="">
                 <div class="newsFlashContainer">
                     <div class="imgContainerIndex">
-                        <img class="secondaryImg" src="./assets-img/catWheel.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
+                        <img class="secondaryImg" src="https://i.ibb.co/cYqm1Zp/stock-photo-futuristic-cyber-cat-in-cyberpunk-style-2205874523-01.jpg" alt="Cat running in a cat wheel having fun... awww how cuuute!">
                     </div>
                     <div class="newsFlashContent">
                         <p class="date">15/06/2599</p>
-                        <h3 class="flashTitle">Cats spotted training on tread mills</h3>
+                        <h3 class="flashTitle">Chats biologiques VS Chats mécaniques :</h3>
                         <p class="newsFlashText">
-                            Cats worldwide have begun rigorous cardiovascular training programs.
-                            Experts say cats have begun these exercises in order to hunt down children more efficiently.
-                            A 30% net increase in child mortality is expected in the years to come.
-                            Cat owners assure the public they're "just playing".
-                            Suspendisse potenti. Praesent justo nisi, consectetur ac velit ac, ornare egestas urna. Praesent ultrices nisl ligula, quis pulvinar arcu facilisis et. Aliquam rhoncus lacus diam, at pharetra tellus ornare at. Curabitur pharetra, orci eget placerat egestas, eros mauris placerat est, sit amet blandit felis erat nec mi. Aliquam erat volutpat. Ut in porttitor velit.
+                        Les chats biologiques et les chats robots sont deux types d'animaux de compagnie très différents. Alors que les chats biologiques sont des animaux vivants,
+            les chats robots sont des animaux de compagnie artificiels conçus pour imiter le comportement des vrais chats.
+            Dans cet article, nous allons explorer les différences entre les chats bio et les chats robots.
+
                         </p>
+                        <form action="article_5.php">
+                            <button class="button_link" type="submit">la suite ici</button>
+                        </form>
                     </div>
                 </div>
             </div>
@@ -460,7 +467,7 @@
         <div class="buttonContainerIndex">
             <button class="buttonIndex">More...</button>
         </div>
-        
+
         <script>
             const cards = document.querySelectorAll("[data-card]");
             cards.forEach(function(elem) {
@@ -474,7 +481,7 @@
     <!-- end of article list -->
 
     <!-- footer starts here -->
-    <button type="button" id="nyanpc"onclick ="colorRainbow(); changeImagepc(); colorainbow2();">&#128008;</button>
+    <button type="button" id="nyanpc" onclick="colorRainbow(); changeImagepc(); colorainbow2();">&#128008;</button>
     <?php include("./assets-php/footer.php"); ?>
 
     <!-- end of footer -->
